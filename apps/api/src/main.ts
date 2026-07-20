23766e483720665dada86a53618c2d1354901f79
neves350 <martimneves350@gmail.com>
2026-07-20 23:39:53 +0100

    feat(api): enable global ValidationPipe

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { apiReference } from '@scalar/nestjs-api-reference'
@@ -5,6 +6,10 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
+
+	app.useGlobalPipes(
+		new ValidationPipe({ transform: true, whitelist: true }),
+	)
 
 	const config = new DocumentBuilder()
 		.setTitle('Aqui Perto API')
