753a00c5cf116661ca8898eba5a9c42fac6426b7
neves350 <martimneves350@gmail.com>
2026-07-21 12:40:34 +0100

    chore(api): remove Nest boilerplate AppController and AppService

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
 import { CarrisModule } from './integrations/carris/carris.module'
-import { StopsModule } from './modules/stops/stops.module';
+import { LinesModule } from './modules/lines/lines.module'
+import { StopsModule } from './modules/stops/stops.module'
 
 @Module({
-	imports: [CarrisModule, StopsModule],
-	controllers: [AppController],
-	providers: [AppService],
+	imports: [CarrisModule, StopsModule, LinesModule],
+	controllers: [],
+	providers: [],
 })
 export class AppModule {}
